a52cd8c8fed64a356cd49d24400e55f1f62c41f1
koprulu555 <androdunyasitvv@gmail.com>
2026-02-28 00:13:04 +0300

    Update default main URL for m3u generator

--- m3u-generator.php
+++ m3u-generator.php
@@ -1,6 +1,6 @@
 <?php
 // Varsayılanlar (fallback - SADECE BAŞARISIZLIK DURUMUNDA)
-$defaultMainUrl = 'https://a.prectv67.lol';
+$defaultMainUrl = 'https://proxy.prectv68.lol/https://a.prectv67.lol';
 $defaultSwKey = '4F5A9C3D9A86FA54EACEDDD635185/c3c5bd17-e37b-4b94-a944-8a3688a30452/';
 $defaultUserAgent = 'okhttp/4.12.0/';
 $defaultReferer = 'https://twitter.com/';
